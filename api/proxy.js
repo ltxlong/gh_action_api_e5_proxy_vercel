46bc43447d6d3431f710d9124ec8d0e362256b22
ltxlong <416803647@qq.com>
2024-12-03 03:09:46 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,44 +1,36 @@
-import querystring from 'querystring';
-
+// api/proxy.js
 export default async function handler(req, res) {
   if (req.method === 'GET' || req.method === 'POST') {
     try {
-      let targetUrl;
-      let restBody;
-
-      // 处理 application/x-www-form-urlencoded 类型的请求
-      if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
-        // 解析请求体
-        const parsedBody = querystring.parse(req.body);
-        targetUrl = parsedBody.targetUrl;
-        restBody = { ...parsedBody };
-        delete restBody.targetUrl; // 删除 targetUrl 以获取剩余参数
-      } else {
-        // 处理 JSON 请求体
-        const { targetUrl: jsonTargetUrl, ...jsonRestBody } = req.body;
-        targetUrl = jsonTargetUrl;
-        restBody = jsonRestBody;
-      }
+      // 解析请求体
+      const { targetUrl, ...restBody } = req.body;
 
       if (!targetUrl) {
         return res.status(400).json({ error: 'targetUrl is required' });
       }
 
-      // 获取原始请求的 headers 和 method
-      const { headers, method } = req;
+      // 获取原始请求的 method
+      const { method } = req;
 
-      // 将剩余的请求参数转换为 x-www-form-urlencoded 格式
-      const urlEncodedBody = querystring.stringify(restBody);
+      if (method === 'POST') {
+       const headers = { 'Content-Type':'application/x-www-form-urlencoded' }
+       const body = {
+        'grant_type': 'refresh_token',
+        'refresh_token': restBody.refresh_token,
+        'client_id': restBody.id,
+        'client_secret': restBody.secret,
+        'redirect_uri': 'http://localhost:53682/'
+       }
+      } else {
+        const headers = { 'Content-Type':'application/json' }
+        const body = undefined
+      }
 
       // 向目标 API 发起请求，并传递剩余的 body 和 headers
       const response = await fetch(targetUrl, {
         method: method,
-        headers: {
-          ...headers,
-          'X-Forwarded-For': req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-          'Content-Type': 'application/x-www-form-urlencoded', // 设置正确的请求头
-        },
-        body: method === 'POST' ? urlEncodedBody : undefined,  // 只在 POST 请求时包含 body
+        headers,
+        body
       });
 
       // 获取目标 API 的响应
@@ -58,3 +50,4 @@ export default async function handler(req, res) {
     res.status(200).send('Hello World');
   }
 }
+
